ad28ddc02d414c9b136fac961006b6d9887558b0
webtech-tom <tom.reisinger@mailbox.org>
2019-08-22 21:25:33 +0200

    Fehler Messages geadded_1

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const router = express.Router();
 
+// User Model
+const User = require('../models/User');
 // Login Seite
 router.get('/login', (req, res) => res.render('login'));
 
@@ -9,10 +12,97 @@ router.get('/login', (req, res) => res.render('login'));
 router.get('/register', (req, res) => res.render('register'));
 
 // Register Handle
-
 router.post('/register', (req, res) => {
-        console.log(req.body);
-        res.send('hallo');
+        const { name, email, password, password2 } = req.body;
+        const errors = [];
+
+        // Felder prüfen, ob Forms gefüllt
+        if (!name || !email || !password || !password2) {
+                errors.push({
+                        msg: 'Bitte alle Felder ausfüllen',
+                });
+        }
+
+        // Checken, ob Passwörter gleich sind
+        if (password != password2) {
+                errors.push({
+                        msg: 'Passwörter stimmen nicht überein',
+                });
+        }
+
+        if (password.length < 6) {
+                errors.push({
+                        msg: 'Passwort muss mindestens 6 Zeichen umfassen',
+                });
+        }
+
+        if (errors.length > 0) {
+                res.render('register', {
+                        errors,
+                        name,
+                        email,
+                        password,
+                        password2,
+                });
+        } else {
+                // Ist die E-Mail bereits registriert?
+                User.findOne({
+                        email,
+                }).then(user => {
+                        // Benutzer bereits registriert
+                        if (user) {
+                                // Error Message
+                                errors.push({
+                                        msg: 'E-Mail wurde bereits registriert',
+                                });
+                                res.render('register', {
+                                        errors,
+                                        name,
+                                        email,
+                                        password,
+                                        password2,
+                                });
+                        } else {
+                                const newUser = new User({
+                                        name,
+                                        email,
+                                        password,
+                                });
+
+                                bcrypt.genSalt(10, (err, salt) => {
+                                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                                                if (err) throw err;
+                                                newUser.password = hash;
+                                                newUser.save()
+                                                        .then(user => {
+                                                                req.flash(
+                                                                        'success_msg',
+                                                                        'Erfolgreich registriert. Du kannst dich nun einloggen'
+                                                                );
+                                                                res.redirect('/users/login');
+                                                        })
+                                                        .catch(err => console.log(err));
+                                        });
+                                });
+                        }
+                });
+        }
+});
+
+// Login
+router.post('/login', (req, res, next) => {
+        passport.authenticate('local', {
+                successRedirect: '/dashboard',
+                failureRedirect: '/users/login',
+                failureFlash: true,
+        })(req, res, next);
+});
+
+// Logout
+router.get('/logout', (req, res) => {
+        req.logout();
+        req.flash('success_msg', 'Ausgeloggt');
+        res.redirect('/users/login');
 });
 
 module.exports = router;
