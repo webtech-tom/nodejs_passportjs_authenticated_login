9b593f883e2e1484456f803c0d3f38f6e9990e5b
webtech-tom <tom.reisinger@mailbox.org>
2019-08-22 20:41:56 +0200

    Views hinzugefuegt

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 // Login Seite
-router.get('/login', (req, res) => res.send('Login'));
+router.get('/login', (req, res) => res.render('login'));
 
 // Registrier Seite
-router.get('/register', (req, res) => res.send('Registrieren'));
+router.get('/register', (req, res) => res.render('register'));
 
 module.exports = router;
