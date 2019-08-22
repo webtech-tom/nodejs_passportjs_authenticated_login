e936e0cf6bf7c1cfbd8e02aa14c0ce05247d26b1
webtech-tom <tom.reisinger@mailbox.org>
2019-08-22 20:59:37 +0200

    registrierung erfolgreich getestet

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
+// Passport Config
+// require('./config/passport')(passport);
+
+// DB Config
+const db = require('./config/keys').MongoURI;
+
+// Verbindung zu MongoDB
+mongoose.connect(db, {
+        useNewUrlParser: true,
+})
+        .then(() => console.log('MongoDB Verbunden'))
+        .catch(err => console.log(err));
+
+// Express body parser
+app.use(
+        express.urlencoded({
+                extended: false,
+        })
+);
 // EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
--- routes/users.js
+++ routes/users.js
@@ -8,4 +8,11 @@ router.get('/login', (req, res) => res.render('login'));
 // Registrier Seite
 router.get('/register', (req, res) => res.render('register'));
 
+// Register Handle
+
+router.post('/register', (req, res) => {
+        console.log(req.body);
+        res.send('hallo');
+});
+
 module.exports = router;
