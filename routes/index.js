91c42df850699df9e7a53c11a4db86f3d81bb182
webtech-tom <38989715+webtech-tom@users.noreply.github.com>
2019-08-23 06:55:24 +0200

    Dashboard Seite hinzugefügt

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const { ensureAuthenticated } = require('../config/auth');
 // Willkommen-Seite
 router.get('/', (req, res) => res.render('willkommen'));
 
-// Dashboard - bei erfolgreichem Login
+// Dashboard - Seite bei erfolgreichem Login
 router.get('/dashboard', ensureAuthenticated, (req, res) =>
         res.render('dashboard', {
                 name: req.user.name,
